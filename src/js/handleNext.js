fafaeffc6548d672c4a7f5b19360f0837cae307b
Caio <kayoroxa@gmail.com>
2022-05-23 21:53:47 -0300

    auto import

--- src/js/handleNext.js
+++ src/js/handleNext.js
@@ -1,7 +1,11 @@
 // const obs = require('../utils/observer')
 const myScript = require('../user/script')
 
-myScript()
+const { Line } = require('../utils/api/line')
+const { Scene, changeTextTo } = require('../utils/api/powerUtils')
+const { Text2 } = require('../utils/api/Text2')
+
+myScript({ Line, Scene, changeTextTo, Text2 })
 
 // function next() {
 //   if (indexScript > myScript.length - 1) {
