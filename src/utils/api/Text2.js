4ec0da47645103d7a0680b89a2e787407d184508
Caio <kayoroxa@gmail.com>
2022-05-23 21:55:19 -0300

    api text 2 changeTextTo

--- src/utils/api/Text2.js
+++ src/utils/api/Text2.js
@@ -47,6 +47,7 @@ function Text2(texts, type = 'div') {
         Element({
           elementHtml: span,
           id: span.id,
+          changeTextTo,
         })
       )
     })
@@ -110,6 +111,69 @@ function Text2(texts, type = 'div') {
           box.innerHTML = currentOnlyText
         }, 200)
       },
+    })
+  }
+
+  function changeTextTo(textAfter) {
+    const textBefore = box.innerHTML
+
+    textBefore.replace(new RegExp('\\s', 'g'), '&nbsp;')
+    const reg = new RegExp(`(${textBefore})`, 'gi')
+    const textBeforeFind = box.innerHTML.match(reg)[0]
+    console.log(box)
+    // const prevOnlyText = box.innerHTML
+
+    // const currentOnlyText = prevOnlyText.replace(reg, textAfter)
+    box.innerHTML = box.innerHTML.replace(/\s/, '&nbsp;')
+    // debugger
+    box.classList.add('text-change')
+
+    box.innerHTML = box.innerHTML.replace(
+      reg,
+      `<div class="before">$1</div>
+      <div class="after" style="opacity: 0">${textAfter}</div>`
+    )
+
+    const beforePreview = get_tex_size(textBeforeFind, '600 60px sans-serif')
+    const nextPreview = get_tex_size(textAfter, '600 60px sans-serif')
+    console.log(beforePreview, nextPreview)
+
+    anime({
+      targets: '.text-change',
+      width: [beforePreview.width, nextPreview.width],
+      easing: 'easeInOutQuad',
+      duration: 250,
+      delay: 100,
+      // duration: 950,
+    })
+    anime.timeline().add({
+      targets: '.text-change .before',
+      opacity: [1, 0],
+      translateY: [0, -beforePreview.height],
+      translateX: [0, -beforePreview.width / 4],
+      // translateX: [0, -beforePreview.width / 2],
+      scale: [1, 0.5],
+      duration: 300,
+      easing: 'easeInOutQuad',
+    })
+    anime({
+      targets: '.text-change .after',
+      delay: 100,
+      opacity: {
+        value: [0, 1],
+        easing: 'easeInOutQuad',
+        duration: 100,
+      },
+      translateY: {
+        value: [beforePreview.height + 40, 0],
+        easing: 'spring(1, 80, 10, 0)',
+      },
+
+      // complete: () => {
+      //   setTimeout(() => {
+      //     box.innerHTML = currentOnlyText
+      //   }, 200)
+      // },
     })
   }
 
