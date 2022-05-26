852c47020940a5caeaf9c43edb395ffa5b4c814e
Caio <kayoroxa@gmail.com>
2022-05-25 22:48:27 -0300

    USER-SCRIPT: update

--- src/user/script-textSize.js
+++ src/user/script-textSize.js
@@ -1,27 +1,47 @@
 const { Enfase, sleep } = require('../utils/api/powerUtils')
 
 module.exports = async ({ Text2, Scene, Line }) => {
-  const myText1 = Text2(['você ', 'tem', ' ', 'feito']).set_x_y({
-    x: 400,
-    y: 300,
+  const myText1 = Text2(['você ', 'tem', ' ', 'feito'])
+    .box_style({
+      // minWidth: '600px',
+      // justifyContent: 'center',
+    })
+    .set_x_y({
+      x: 'center',
+      y: 'center',
+    })
+
+  const myText3 = Text2(['have you', ' ', 'ever']).set_x_y({
+    x: 'center',
+    y: 450,
   })
 
   Scene.show(myText1)
+  Scene.show(myText3)
 
-  const enfase = Enfase(myText1.children, '60px', '100px')
+  const line1 = Line(myText1.children[1], { padding: 0 })
+
+  Scene.show(line1.animate())
+
+  const enfase = Enfase(myText1.children, '30px', '60px')
 
   await Scene.playClicks([
     //
-    () => enfase.select(0),
+    () => {
+      line1.move_animate_to(myText1.children[2])
+      // enfase.select(0)
+
+      myText1.children[0].style.color = '#ff0000'
+    },
     () => enfase.select(1),
     () => enfase.select(3),
   ])
 
   await sleep(600)
 
-  const line1 = Line(myText1.children[1], { padding: 0 })
+  // const line1 = Line(myText1.children[1], { padding: 0 })
 
-  await Scene.playClick(() => {
-    Scene.show(line1.animate())
-  })
+  // await Scene.playClick(() => {
+  //   Scene.show(line1.animate())
+  // })
 }
