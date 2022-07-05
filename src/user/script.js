8a612022ea976a7fa8b838163039c6c953c0d1b0
Caio <kayoroxa@gmail.com>
2022-07-05 02:45:27 -0300

    script refact name

--- src/user/script.js
+++ src/user/script.js
@@ -6,9 +6,9 @@ const nextTo = require('./script-nextTo') //bug
 const textSize = require('./script-textSize') //bug
 const traduçãoParcelada = require('./tradução-parcelada') //bug
 
-const sublinhar = require('./script-sublinhar')
-const bible = require('./bible')
-const scriptAudioTextoVideo = require('./script-audio-texto-video')
+const lineAndSublinhar = require('./script-sublinhar')
+const line = require('./bible')
+const lineWalking = require('./script-audio-texto-video')
 const textMorph = require('./script-text-morph')
 const morphLetters = require('./script-morph-letters')
 const pronounce = require('./script-pronounce')
