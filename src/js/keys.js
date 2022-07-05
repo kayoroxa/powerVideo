1dd835cd91685d5764670d338aa221c760d55da3
Caio <kayoroxa@gmail.com>
2022-07-05 02:48:06 -0300

    backspace clean scene

--- src/js/keys.js
+++ src/js/keys.js
@@ -4,4 +4,7 @@ document.addEventListener('keydown', e => {
   if (e.key === 'Enter') {
     obs('CONTROL').notify('next')
   }
+  if (e.key === 'Backspace') {
+    document.querySelector('.app').innerHTML = ''
+  }
 })
